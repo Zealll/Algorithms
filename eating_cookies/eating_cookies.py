b9f27b4d6778bb127c614828051298bbb91d81a6
Zealll <eriznis@gmail.com>
2020-01-22 19:10:19 -0500

    Updated Stock_Prices

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,9 +8,15 @@ import sys
 def eating_cookies(n, cache=None):
   pass
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+print(eating_cookies(0), 1)
+print(eating_cookies(1), 1)
+print(eating_cookies(2), 2)
+print(eating_cookies(5), 13)
+print(eating_cookies(10), 274)
+
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_cookies = int(sys.argv[1])
+#     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+#   else:
+#     print('Usage: eating_cookies.py [num_cookies]')
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -5,36 +5,11 @@ import argparse
 def find_max_profit(prices):
   profit = prices[1] - prices[0]
   for i in range(1, len(prices)):
-      j = 0
-      while j < i:
-          # print(profit)
+      for j in range(0, i):
           if prices[i] - prices[j] > profit:
               profit = prices[i] - prices[j]
-          j += 1
+              
   return profit
-  # for i in range(1, len(prices)):
-  #   if prices[i] - prices[i - 1] > highest:
-  #     highest = prices[i] - lowest
-  #     print(highest)
-  #     print('lowest',lowest)
-  #     if lowest > prices[i]:
-  #       lowest = prices[i]
-      # print(highest)
-  # for i in range(len(prices)):
-  #   # if i <= len(prices) - 2 and lowest < prices[i+1] and lowest > prices[i] and lowest > prices[i-1]:
-  #   if i <= len(prices) - 2 and prices[i] < prices[i+1] and prices[i] < prices[i-1]:
-  #     lowest = prices[i]
-  #     index = i
-  #     # print(prices[i])
-
-  # for i in range(index, len(prices)):
-  #   if i <= len(prices) - 2 and prices[i] < prices[i+1] and highest < prices[i + 1]:
-  #     highest = prices[i + 1]
-  #     print(highest)
-  
-  # print(highest, lowest)
-  # return highest - lowest
-  # return highest
 print(find_max_profit([10, 7, 5, 8, 11, 9]))
 print(find_max_profit([100, 90, 80, 50, 20, 10]))
 print(find_max_profit([1050, 270, 1540, 3800, 2]))
